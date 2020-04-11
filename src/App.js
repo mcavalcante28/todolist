64bbe4595df5fa8fff7616251fe35b4be103d2b0
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-10 21:35:06 -0300

    Implementando o estado de todos e visualização simples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import './styles.css'
 function App() {
+
+  const [writeToDo, setWriteToDo] = useState('');
+  const [toDo, setToDo] = useState(['Initia']);
+
+
+  function addTodo(e){
+    e.preventDefault();
+    setToDo([...toDo, writeToDo]);
+  }
+
+  function endTodo(index){
+    delete toDo[index];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full-container">
+      <form onSubmit={addTodo}>
+        <input value={writeToDo} onChange={e => setWriteToDo(e.target.value)}>
+        </input>
+        <button type='submit'>
+          Add Todo
+        </button>
+      </form>
+      <div>
+        <div>
+        {toDo.map((todo, index) => (
+          <div>
+          <input type="checkbox"> 
+          </input>
+          {todo}
+          <button onClick={endTodo(index)}></button>
+          </div>
+        ))}
+
+        </div>
+      </div>
+
     </div>
   );
 }
