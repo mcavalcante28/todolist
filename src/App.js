5c9d62767719fc0759dd6faa416d28da1689a6d5
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-13 11:01:46 -0300

    Adicionando mudanças de estilização

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import { FiTrash2 } from 'react-icons/fi';
 import './styles.css'
+import {format} from 'date-fns'
 
 
 
 function App() {
 
-  const [writeToDo, setWriteToDo] = useState([localStorage.getItem('todos')]);
+  const [writeToDo, setWriteToDo] = useState([]);
   const [toDo, setToDo] = useState([]);
-  const [completedList, setCompletedList] = useState([localStorage.getItem('completeds')]);
+  const [completedList, setCompletedList] = useState([]);
 
   function addTodo(e){
     e.preventDefault();
-    setToDo([...toDo, {text: writeToDo, completed: false}]);
+    setToDo([...toDo, {text: writeToDo, completed: false, date: format(new Date(), 'dd.MM.yy')}]);
   }
 
   function deleteTodo(toDoIndex){   
@@ -35,33 +36,38 @@ function App() {
   return (
     <div className="full-container">
       <form onSubmit={addTodo}>
+        <p> To do App </p>
         <input value={writeToDo} onChange={e => setWriteToDo(e.target.value)}>
         </input>
         <button type='submit'>
           Add Todo
         </button>
       </form>
-      <div>
-        <div>
+      <div className="bottom-container">
+        <div className="todo-list"> 
+        <p> Lista por fazer </p>
         {toDo.map((todo, index) => (
           <div> 
             {!toDo[index].completed ? (
-              <div key={index}>
+              <div className="todo-label" key={index}>
               <p onClick={() => completeTodo(index)} 
               className={"notCompleted_todo"}>{todo.text}</p>
-              <button onClick={() => deleteTodo(index)}>
-              <FiTrash2 size={15} color="#a8a8b3"/>
+              <button className="delete-button" onClick={() => deleteTodo(index)}>
+              <FiTrash2 size={15} color="#000"/>
               </button>
               </div>
             ) :null}
             </div>
         ))}
+        </div>
+        <div className="todo-list">
+        <p> Lista completos </p>
          {toDo.map((todo, index) => (
           <div> 
             {toDo[index].completed ? (
               <div className="completed-container" key={index}>
               <p onClick={() => completeTodo(index)} 
-              >{todo.text}</p>
+              >{todo.text}</p> 
               <button onClick={() => deleteTodo(index)}>
               <FiTrash2 size={15} color="#a8a8b3"/>
               </button>
@@ -69,11 +75,10 @@ function App() {
             ) :null}
             </div>
         ))}
+        </div>
 
         </div>
       </div>
-
-    </div>
   );
 }
 
